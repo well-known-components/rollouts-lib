e8f31435c1d6439878243f71dee59caf3f3f9ce3
Agustin Mendez <github@menduz.com>
2021-08-20 19:00:27 -0300

    initial release

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,148 @@
+import * as murmurHash3 from "murmurhash3js"
+import { compare, valid } from "semver"
+
 /**
- * A function that does something
  * @public
  */
-export function example(){
-  return true
+export type RolloutName = string
+
+/**
+ * @public
+ */
+export type RolloutVersion = string
+
+/**
+ * @public
+ */
+export type RolloutDomain = {
+  records: Record<RolloutName, RolloutRecord[]>
+}
+
+/**
+ * @public
+ */
+export type RolloutRecord = {
+  version: string
+  percentage: number
+  prefix: string
+
+  updatedAt?: number
+  createdAt?: number
+}
+
+/**
+ * User context used to calculate rollouts.
+ * @public
+ */
+export type Context = {
+  userId?: string
+  sessionId?: string
+  remoteAddress?: string
+}
+
+/**
+ * This function calculates a normalized value for a RolloutRecord and a Context.
+ * The function is stable and deterministic for non-empty contexts,
+ * it returns a numeric hash from 1 to 100.
+ * @public
+ */
+export function normalizedValue(context: Context, rollout: RolloutRecord): number {
+  const userId = context.userId || context.sessionId || context.remoteAddress || Math.random()
+  const rolloutId = `${rollout.version}:${rollout.prefix}`
+  return (murmurHash3.x86.hash32(`${rolloutId}:${userId}`) % 100) + 1
+}
+
+/**
+ * Calculates and selects a rollout for a list of RolloutRecords and a given
+ * Context.
+ *
+ * @public
+ */
+export function calculateRollout(context: Context, records: RolloutRecord[]): RolloutRecord {
+  if (records.length == 0) {
+    throw new Error("Empty rollouts")
+  }
+  for (let rollout of records) {
+    const normalizedUserId = normalizedValue(context, rollout)
+    if (rollout.percentage > 0 && normalizedUserId <= rollout.percentage) {
+      return rollout
+    }
+  }
+  // return last as fallback
+  return records[records.length - 1]
+}
+
+/**
+ * Calculates all rollouts for a specific RolloutDomain and Context
+ *
+ * @public
+ */
+export function calculateRolloutsForDomain(
+  context: Context,
+  domain: Partial<RolloutDomain>
+): Record<string, RolloutRecord> {
+  const map: Record<string, RolloutRecord> = {}
+
+  if (domain.records) {
+    for (let rolloutName of Object.keys(domain.records)) {
+      if (domain.records[rolloutName] && domain.records[rolloutName].length) {
+        map[rolloutName] = calculateRollout(context, domain.records[rolloutName])
+      }
+    }
+  }
+
+  return map
+}
+
+/**
+ * @public
+ */
+export function patchRollouts(
+  currentValues: Partial<RolloutDomain>,
+  rolloutName: string,
+  patch: Pick<RolloutRecord, "percentage" | "prefix" | "version">,
+  timestamp: number = Date.now()
+): Partial<RolloutDomain> {
+  const { version, percentage, prefix } = patch
+
+  if (typeof rolloutName != "string" || !rolloutName.length) {
+    throw new Error("patchRollouts: invalid rolloutName")
+  }
+  if (typeof version != "string" || !version.length || !valid(version)) {
+    throw new Error("patchRollouts: invalid version: " + version)
+  }
+  if (typeof percentage != "number" || isNaN(percentage) || percentage < 0 || percentage > 100) {
+    throw new Error(`patchRollouts: invalid percentage ${percentage}`)
+  }
+  if (typeof timestamp != "number" || isNaN(timestamp) || timestamp < 0) {
+    throw new Error(`patchRollouts: invalid timestamp ${timestamp}`)
+  }
+
+  // always normalize for backwards compatibility
+  currentValues.records = currentValues.records || {}
+  currentValues.records[rolloutName] = currentValues.records[rolloutName] || []
+
+  // apply changes
+  let currentVersion = currentValues.records[rolloutName]!.find(($) => $.version == version)
+
+  if (!currentVersion) {
+    currentVersion = {
+      percentage: percentage | 0,
+      prefix,
+      version,
+      createdAt: timestamp,
+    }
+    currentValues.records[rolloutName]!.push(currentVersion)
+  }
+
+  // override currentVersion percentage
+  currentVersion.percentage = percentage
+  currentVersion.updatedAt = timestamp
+
+  // sort deployments before saving
+  currentValues.records[rolloutName]!.sort((a, b) => {
+    return -compare(a.version, b.version)
+  })
+
+  return currentValues
 }
--- test/unit.spec.ts
+++ test/unit.spec.ts
@@ -1,8 +1,187 @@
-import { example } from "../src"
+import { normalizedValue, calculateRollout, patchRollouts, calculateRolloutsForDomain, RolloutDomain } from "../src"
 import expect from "expect"
 
-describe("unit", () => {
-  it("smoke test", () => {
-    expect(example()).toEqual(true)
+describe("normalizedValue", () => {
+  it("deterministicness userId", () => {
+    const a1 = normalizedValue({ userId: "a" }, { percentage: 0, prefix: "", version: "" })
+    const a2 = normalizedValue({ userId: "a" }, { percentage: 0, prefix: "", version: "" })
+    const b1 = normalizedValue({ userId: "b" }, { percentage: 0, prefix: "", version: "" })
+    expect(a1).toEqual(a2)
+    expect(a1).not.toEqual(b1)
+  })
+
+  it("deterministicness sessionId", () => {
+    const a1 = normalizedValue({ sessionId: "a" }, { percentage: 0, prefix: "", version: "" })
+    const a2 = normalizedValue({ sessionId: "a" }, { percentage: 0, prefix: "", version: "" })
+    const b1 = normalizedValue({ sessionId: "b" }, { percentage: 0, prefix: "", version: "" })
+    expect(a1).toEqual(a2)
+    expect(a1).not.toEqual(b1)
+  })
+
+  it("deterministicness remoteAddress", () => {
+    const a1 = normalizedValue({ remoteAddress: "a" }, { percentage: 0, prefix: "", version: "" })
+    const a2 = normalizedValue({ remoteAddress: "a" }, { percentage: 0, prefix: "", version: "" })
+    const b1 = normalizedValue({ remoteAddress: "b" }, { percentage: 0, prefix: "", version: "" })
+    expect(a1).toEqual(a2)
+    expect(a1).not.toEqual(b1)
+  })
+
+  it("non deterministicness empty context", () => {
+    const a = normalizedValue({}, { percentage: 0, prefix: "", version: "" })
+    const b = normalizedValue({}, { percentage: 0, prefix: "", version: "" })
+    const c = normalizedValue({}, { percentage: 0, prefix: "", version: "" })
+    expect(a).not.toEqual(b)
+    expect(b).not.toEqual(c)
+  })
+})
+
+describe("calculateRollout", () => {
+  it("throws if empty rollouts", () => {
+    expect(() => calculateRollout({}, [])).toThrow()
+  })
+
+  it("selects the last one every time, even if percentage is zero", () => {
+    const rollout = { percentage: 1, prefix: "", version: "" }
+    expect(calculateRollout({}, [rollout])).toEqual(rollout)
+  })
+
+  it("selects the last one every time, even if percentage is zero", () => {
+    const rollout = { percentage: 0, prefix: "", version: "" }
+    expect(calculateRollout({}, [rollout])).toEqual(rollout)
+  })
+})
+
+describe("calculateRollout", () => {
+  it("throws if empty rollouts", () => {
+    expect(() => calculateRollout({}, [])).toThrow()
+  })
+
+  it("selects the last one every time, even if percentage is zero", () => {
+    const rollout = { percentage: 1, prefix: "", version: "" }
+    expect(calculateRollout({}, [rollout])).toEqual(rollout)
+  })
+
+  it("selects the last one every time, even if percentage is zero", () => {
+    const rollout = { percentage: 0, prefix: "", version: "" }
+    expect(calculateRollout({}, [rollout])).toEqual(rollout)
+  })
+})
+
+describe("patchRollouts", () => {
+  it("creation from empty domain", () => {
+    const result = patchRollouts({}, "_site", { prefix: "package", percentage: 10, version: "0.1.1" }, 123)
+
+    expect(result.records).toEqual({
+      _site: [{ percentage: 10, prefix: "package", version: "0.1.1", updatedAt: 123, createdAt: 123 }],
+    })
+  })
+
+  it("should replace percentage of existing rollout", () => {
+    let result = patchRollouts({}, "_site", { prefix: "package", percentage: 10, version: "0.1.1" }, 123)
+    result = patchRollouts(result, "_site", { prefix: "package", percentage: 99, version: "0.1.1" }, 124)
+
+    expect(result.records).toEqual({
+      _site: [{ percentage: 99, prefix: "package", version: "0.1.1", updatedAt: 124, createdAt: 123 }],
+    })
+  })
+
+  it("should add new versions", () => {
+    let result = patchRollouts({}, "_site", { prefix: "package", percentage: 10, version: "0.1.1" }, 123)
+    result = patchRollouts(result, "_site", { prefix: "package-beta", percentage: 99, version: "0.0.1-beta.test" }, 124)
+    result = patchRollouts(result, "_site", { prefix: "package", percentage: 100, version: "0.1.2" }, 125)
+
+    expect(result.records).toEqual({
+      _site: [
+        { percentage: 100, prefix: "package", version: "0.1.2", updatedAt: 125, createdAt: 125 },
+        { percentage: 10, prefix: "package", version: "0.1.1", updatedAt: 123, createdAt: 123 },
+        { percentage: 99, prefix: "package-beta", version: "0.0.1-beta.test", updatedAt: 124, createdAt: 124 },
+      ],
+    })
+  })
+
+  it("should add new versions", () => {
+    let result = patchRollouts({}, "_site", { prefix: "package", percentage: 10, version: "0.1.1" }, 123)
+    result = patchRollouts(result, "_site", { prefix: "package-beta", percentage: 99, version: "0.0.1-beta.test" }, 124)
+    result = patchRollouts(result, "_site", { prefix: "package", percentage: 100, version: "0.1.2" }, 125)
+    result = patchRollouts(result, "@a/a", { prefix: "@a/a", percentage: 100, version: "3.1.2" }, 126)
+
+    expect(result.records).toEqual({
+      _site: [
+        { percentage: 100, prefix: "package", version: "0.1.2", updatedAt: 125, createdAt: 125 },
+        { percentage: 10, prefix: "package", version: "0.1.1", updatedAt: 123, createdAt: 123 },
+        { percentage: 99, prefix: "package-beta", version: "0.0.1-beta.test", updatedAt: 124, createdAt: 124 },
+      ],
+      "@a/a": [{ percentage: 100, prefix: "@a/a", version: "3.1.2", updatedAt: 126, createdAt: 126 }],
+    })
+  })
+
+  it("should fail on invalid percentages", () => {
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: 1000, version: "0.1.1" }, 123)).toThrow()
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: -100, version: "0.1.1" }, 123)).toThrow()
+    expect(() =>
+      patchRollouts({}, "_site", { prefix: "package", percentage: "asd" as any, version: "0.1.1" }, 123)
+    ).toThrow()
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: 101, version: "0.1.1" }, 123)).toThrow()
+    expect(() =>
+      patchRollouts({}, "_site", { prefix: "package", percentage: "" as any, version: "0.1.1" }, 123)
+    ).toThrow()
+    expect(() =>
+      patchRollouts({}, "_site", { prefix: "package", percentage: null as any, version: "0.1.1" }, 123)
+    ).toThrow()
+  })
+
+  it("should fail on invalid versions", () => {
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "watermelon" }, 123)).toThrow()
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "" }, 123)).toThrow()
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "asd-0.1.1" }, 123)).toThrow()
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: null as any }, 123)).toThrow()
+    expect(() =>
+      patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: new Date() as any }, 123)
+    ).toThrow()
+  })
+
+  it("should fail on invalid rolloutName", () => {
+    expect(() => patchRollouts({}, "", { prefix: "package", percentage: 1, version: "1.1.1" }, 123)).toThrow()
+    expect(() => patchRollouts({}, 123 as any, { prefix: "package", percentage: 1, version: "1.1.1" }, 123)).toThrow()
+    expect(() => patchRollouts({}, null as any, { prefix: "package", percentage: 1, version: "1.1.1" }, 123)).toThrow()
+    expect(() => patchRollouts({}, {} as any, { prefix: "package", percentage: 1, version: "1.1.1" }, 123)).toThrow()
+  })
+
+  it("should fail on invalid timestamp", () => {
+    expect(() =>
+      patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "1.1.1" }, null as any)
+    ).toThrow()
+    expect(() =>
+      patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "1.1.1" }, "123" as any)
+    ).toThrow()
+    expect(() =>
+      patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "1.1.1" }, {} as any)
+    ).toThrow()
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "1.1.1" }, 0)).toThrow()
+    expect(() => patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "1.1.1" }, -123)).toThrow()
+
+    expect(
+      patchRollouts({}, "_site", { prefix: "package", percentage: 1, version: "1.1.1" }).records._site[0].createdAt
+    ).toBeGreaterThan(0)
+  })
+})
+
+describe("integration", () => {
+  it("should calculate rollotus with 100%", () => {
+    let domain = patchRollouts({}, "_site", { prefix: "package", percentage: 10, version: "0.1.1" }, 123)
+    domain = patchRollouts(domain, "_site", { prefix: "package-beta", percentage: 99, version: "0.0.1-beta.test" }, 124)
+    domain = patchRollouts(domain, "_site", { prefix: "package", percentage: 100, version: "0.1.2" }, 125)
+    domain = patchRollouts(domain, "@a/a", { prefix: "@a/a", percentage: 100, version: "3.1.2" }, 126)
+
+    expect(calculateRolloutsForDomain({}, domain)).toEqual({
+      _site: { percentage: 100, prefix: "package", version: "0.1.2", updatedAt: 125, createdAt: 125 },
+      "@a/a": { percentage: 100, prefix: "@a/a", version: "3.1.2", updatedAt: 126, createdAt: 126 },
+    })
+  })
+  it("should calculate rollotus even if the domain is malformed", () => {
+    expect(calculateRolloutsForDomain({}, {})).toEqual({})
+  })
+  it("should calculate rollotus even if the domain is malformed with empty arrays", () => {
+    expect(calculateRolloutsForDomain({}, { records: { _site: [] } })).toEqual({})
   })
 })
